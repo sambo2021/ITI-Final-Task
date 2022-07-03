bd154db35ea70f6e04f188227e690f43742649d8
Abdurrhman Sabry <abdurrhmansabry@gmail.com>
2022-07-03 18:51:56 +0200

    Removing the extra file for config

--- Build.sh
+++ Build.sh
@@ -1,9 +1,10 @@
 #! /bin/bash 
 if [[ ! -f "./Minikube-Infra/TF_key.pem" ]]; then
     touch ./Minikube-Infra/TF_key.pem
-    chmod 400 ./Minikube-Infra/TF_key.pem
+    #chmod 400 ./Minikube-Infra/TF_key.pem
     touch ./Ansible-Credentials/inventory
     cd ./Minikube-Infra
+    terraform init
     terraform apply -auto-approve
     cd ../Ansible-Credentials
     ansible-playbook -i inventory playbook.yaml --private-key ../Minikube-Infra/TF_key.pem -u ubuntu --ssh-common-args='-o StrictHostKeyChecking=no' --verbose
--- Destroy.sh
+++ Destroy.sh
@@ -7,5 +7,6 @@ if [[  -f "./Minikube-Infra/TF_key.pem" ]]; then
     rm -f ./Kubernetes-Resources/client.crt
     rm -f ./Kubernetes-Resources/client.key
     rm -f ./Kubernetes-Resources/ca.crt
+    rm -f ./Kubernetes-Resources/config
 fi
 
