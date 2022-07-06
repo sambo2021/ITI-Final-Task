1d7c66a9a303ad1205d12a56e08971e096e3e171
sambo2021 <mohamedsambomahmoud@gmail.com>
2022-07-06 02:31:14 +0200

    updated

--- Destroy.sh
+++ Destroy.sh
@@ -1,9 +1,13 @@
 if [[  -f "./Minikube-Infra/TF_key.pem" ]]; then
-    cd ./Minikube-Infra
+    cd ./Kubernetes-Resources
+    terraform destroy -aut-approve
+
+    cd ../Minikube-Infra
     terraform destroy -auto-approve
     cd ..
     rm -f ./Minikube-Infra/TF_key.pem
     rm -f ./Ansible-Credentials/inventory
+    rm -f ./Get-Passwords/inventory
     rm -f ./Kubernetes-Resources/client.crt
     rm -f ./Kubernetes-Resources/client.key
     rm -f ./Kubernetes-Resources/ca.crt
