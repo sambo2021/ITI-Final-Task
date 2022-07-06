2728dbe3dfc69b3131ad994b89a98169fcdc41dc
sambo2021 <mohamedsambomahmoud@gmail.com>
2022-07-06 02:41:54 +0200

    updated

--- Build.sh
+++ Build.sh
@@ -3,6 +3,7 @@ if [[ ! -f "./Minikube-Infra/TF_key.pem" ]]; then
     # creating empty key and inventory
     touch ./Minikube-Infra/TF_key.pem
     touch ./Ansible-Credentials/inventory
+    touch ./Get-Passwords/inventory
     # building minikube cluster locally and using its local exec 
     # to set ec2 ip to inventory in ../Ansible-Credentials/inventory and kubernetes provider in ../Kubernetes-Resources/main.tf
     cd ./Minikube-Infra
